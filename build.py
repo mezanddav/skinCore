a6493c95e92b1a62443796dcad49cbae3ba6ba69
David Mezei <mezanddav@gmail.com>
2022-07-22 10:25:26 -0400

    Small build changes

--- build.py
+++ build.py
@@ -1,13 +1,12 @@
 import zipfile
 import json
 
-release_manifest = open('manifest.json')
-release_files = ['skincore.js', 'skincore.css', 'manifest.json']
-
-release_version = json.load(release_manifest)['version']
-release_name = 'release-' + release_version + '.zip'
+manifest = open('manifest.json')
+manifest = json.load(manifest)
+release_name = manifest['name'] + '-release-' + manifest['version'] + '.zip'
 release_path = 'releases/' + release_name
 
+release_files = ['skincore.js', 'skincore.css', 'manifest.json']
 with zipfile.ZipFile(release_path, 'w') as zipF:
     for file in release_files:
         zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
